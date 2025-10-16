ef2ff67a0c15518b710f5d9e11fceb2fa146687d
H1007_04 <3120645832@qq.com>
2025-10-16 17:58:47 +0800

    绝对路径的学习

--- files/file-test.js
+++ files/file-test.js
@@ -1,13 +1,15 @@
 const fs = require('fs')
+// 路径拼接尽量不使用相对路径，使用绝对路径更加安全。（避免出现动态拼接路径错误）
+console.log(__dirname);   // 当前模块的目录路径
+console.log(__filename);  // 当前模块的文件路径
 
-fs.readFile('./filesTxt/成绩.txt', 'utf8', (err, data) => {
+fs.readFile(__dirname + '/filesTxt/成绩.txt', 'utf8', (err, data) => {
     if (err) {
         console.error(err)
         return
     }
-    let succesData = []
 
-    succesData = data.split(' ').map(item => {
+    let succesData = data.split(' ').map(item => {
         let list = item.split('=')
         return { 
             [list[0]]: parseInt(list[1])
@@ -15,7 +17,7 @@ fs.readFile('./filesTxt/成绩.txt', 'utf8', (err, data) => {
     })
     let newData = JSON.stringify(succesData)
     
-    fs.writeFile('./filesTxt/新成绩.txt', newData, 'utf8', (err2)=>{
+    fs.writeFile(__dirname + '/filesTxt/新成绩.txt', newData, 'utf8', (err2)=>{
         if(err2){
             console.error(err2)
         }
